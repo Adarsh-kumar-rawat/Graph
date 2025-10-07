4fbf89f995b7964fb1d18a837aced8a225887b68
Adarsh-kumar-rawat <adarsh0489@gmail.com>
2025-10-07 12:03:18 +0530

    Adjacency List For graph

--- Graph.py
+++ Graph.py
@@ -1,3 +1,19 @@
 v = 6
 e = 7
 sett = [(0,1),(0,3),(0,4),(1,2),(1,5),(2,4),(3,4)]
+
+# ADJACENCY LIST 
+adjList = []
+
+for i in range(v):
+    adjList.append([])
+
+for edge in sett:
+    x = edge[0]
+    y = edge[1]
+
+    adjList[x].append(y)
+    adjList[y].append(x)
+
+for i in range(v):
+    print(i ,"->",adjList[i])
